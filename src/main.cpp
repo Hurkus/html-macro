4c07072e3dbe7439ae83661b564dd8c13ffa17e6
Hurkus <8364277+Hurkus@users.noreply.github.com>
2025-04-01 12:44:06 +0200

    Initial commit.

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "DEBUG.hpp"
+
+using namespace std;
+
+
+// --------------------------------- [ Main Function ] -------------------------------------- //
+
+
+int main(int argc, char const* const* argv){
+	printf(ANSI_BOLD "Hello world!" ANSI_RESET "\n");
+	return 0;
+}
+
+
+// ------------------------------------------------------------------------------------------ //
