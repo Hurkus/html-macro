68ab171c6547a1952516284b2893875e11b8928c
Hurkus <8364277+Hurkus@users.noreply.github.com>
2026-03-21 13:37:57 +0100

    Added iterator to str_map.

--- src/includes/str_map.hpp
+++ src/includes/str_map.hpp
@@ -20,6 +20,9 @@ public:
 	using mapped_type = std::unique_ptr<entry>;
 	using map_type = std::unordered_map<key_type,mapped_type>;
 	
+	struct iterator;
+	struct const_iterator;
+	
 // ------------------------------------[ Properties ] --------------------------------------- //
 private:
 	map_type map;
@@ -35,7 +38,7 @@ public:
 		// Created, create key-value pair
 		if (p.second){
 			rEntry = makeEntry(key);
-			(key_type&)rKey = key_type(rEntry->key, key.length());
+			const_cast<key_type&>(rKey) = key_type(rEntry->key, key.length());
 			new (&rEntry->value) value_type(std::forward<ARG>(args)...);
 		} else {
 			rEntry->value = value_type(std::forward<ARG>(args)...);
@@ -53,7 +56,7 @@ public:
 		// Created, create key-value pair
 		if (p.second){
 			rEntry = makeEntry(key);
-			(key_type&)rKey = key_type(rEntry->key, key.length());
+			const_cast<key_type&>(rKey) = key_type(rEntry->key, key.length());
 			new (&rEntry->value) value_type();
 		}
 		
@@ -63,6 +66,14 @@ public:
 	
 // ----------------------------------- [ Functions ] ---------------------------------------- //
 public:
+	bool empty() const noexcept {
+		return map.empty();
+	}
+	
+	size_t size() const noexcept {
+		return map.size();
+	}
+	
 	entry* find(const key_type& key){
 		auto p = map.find(key);
 		if (p != nullptr)
@@ -102,13 +113,38 @@ public:
 	}
 	
 // ----------------------------------- [ Functions ] ---------------------------------------- //
+public:
+	void copy(str_map<T>& dst){
+		for (const auto& p : map){
+			assert(p.second != nullptr);
+			dst.insert(p.first, p.second->value);
+		}
+	}
+	
+	iterator begin(){
+		return iterator(map.begin());
+	}
+	
+	const_iterator begin() const {
+		return const_iterator(map.begin());
+	}
+	
+	iterator end(){
+		return iterator(map.end());
+	}
+	
+	const_iterator end() const {
+		return const_iterator(map.end());
+	}
+	
+// ----------------------------------- [ Functions ] ---------------------------------------- //
 private:
 	mapped_type makeEntry(const key_type& key){
 		const size_t size = sizeof(entry) + sizeof(char)*(key.length() + 1);
 		entry* e = (entry*)::operator new(size, std::align_val_t(alignof(entry::value)));
 		
 		// Copy key
-		char* key_buff = (char*)e->key;
+		char* key_buff = const_cast<char*>(e->key);
 		std::copy(key.data(), key.data() + key.length(), key_buff);
 		key_buff[key.length()] = 0;
 		
@@ -116,4 +152,80 @@ private:
 	}
 	
 // ------------------------------------------------------------------------------------------ //
+};
+
+
+
+template<typename T>
+struct str_map<T>::iterator : public str_map<T>::map_type::iterator {
+// ---------------------------------------------------------------- //
+public:
+	using map_iterator = str_map<T>::map_type::iterator;
+	
+	struct Pair {
+		std::string_view key;
+		T& value;
+	};
+	
+// ---------------------------------------------------------------- //
+public:
+	iterator() = default;
+	iterator(const auto& p) : map_iterator(p) {}
+	iterator(auto&& p) : map_iterator(std::move(p)) {}
+	
+// ---------------------------------------------------------------- //
+public:
+	T* operator->() const {
+		auto entry = static_cast<const map_iterator&>(*this).operator->();
+		return entry->second->value;
+		
+	}
+	
+	Pair operator*() const {
+		auto entry = static_cast<const map_iterator&>(*this).operator->();
+		return Pair {
+			.key = entry->first,
+			.value = entry->second->value
+		};
+	}
+	
+// ---------------------------------------------------------------- //
+};
+
+
+
+template<typename T>
+struct str_map<T>::const_iterator : public str_map<T>::map_type::const_iterator {
+// ---------------------------------------------------------------- //
+public:
+	using map_iterator = str_map<T>::map_type::const_iterator;
+	
+	struct Pair {
+		std::string_view key;
+		const T& value;
+	};
+	
+// ---------------------------------------------------------------- //
+public:
+	const_iterator() = default;
+	const_iterator(const auto& p) : str_map<T>::map_type::const_iterator(p) {}
+	const_iterator(auto&& p) : str_map<T>::map_type::const_iterator(std::move(p)) {}
+	
+// ---------------------------------------------------------------- //
+public:
+	const T* operator->() const {
+		auto entry = static_cast<const map_iterator&>(*this).operator->();
+		return entry->second->value;
+		
+	}
+	
+	Pair operator*() const {
+		auto entry = static_cast<const map_iterator&>(*this).operator->();
+		return Pair {
+			.key = entry->first,
+			.value = entry->second->value
+		};
+	}
+	
+// ---------------------------------------------------------------- //
 };
